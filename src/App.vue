aa08f52a6b187bddc59ee6e56323da0ed4f69879
samkallon <39991473+samkallon@users.noreply.github.com>
2022-10-10 14:06:27 +0800

    simple layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <left-menu class="left-menu"></left-menu>
+    <CesiumViewer class="cesium-viewer"></CesiumViewer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeftMenu from "@/components/LeftMenu";
+import CesiumViewer from "@/components/CesiumViewer";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CesiumViewer,
+    LeftMenu
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  .left-menu{
+    flex-grow: 1;
+  }
+  .cesium-viewer{
+    flex-grow: 10;
+  }
 }
 </style>
---
+++ src/components/CesiumViewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cesium-viewer"></div>
+</template>
+
+<script>
+export default {
+  name: "CesiumViewer"
+}
+</script>
+
+<style scoped>
+  .cesium-viewer{
+    height: 100%;
+    border: 1px solid red;
+  }
+</style>
